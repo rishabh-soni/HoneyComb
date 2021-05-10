487d517dd385c02ed52ffd2bfbc5921ba470db62
rishabh-soni <rishabh.rs.001@gmail.com>
2021-05-11 01:56:20 +0530

    Made test cases for authen and products app.

--- authen/tests.py
+++ authen/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import *
+
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        CustomUser.objects.create(full_name='Sponge Bob', phone_no='1234567890', email='abc@xyz.com')
+
+    def test_full_name_label(self):
+        user = CustomUser.objects.get(id=1)
+        field_label = user._meta.get_field('full_name').verbose_name
+        self.assertEqual(field_label, 'full name')
+
+    def test_phone_no_label(self):
+        user = CustomUser.objects.get(id=1)
+        field_label = user._meta.get_field('phone_no').verbose_name
+        self.assertEqual(field_label, 'phone no')
+
+    def test_full_name_max_length(self):
+        user = CustomUser.objects.get(id=1)
+        max_length = user._meta.get_field('full_name').max_length
+        self.assertEqual(max_length, 100)
+
+
+class SignUpFormTest(TestCase):
+    def test_full_name_field_label(self):
+        form = SignUpForm()
+        self.assertTrue(form.fields['full_name'].label is None or form.fields['full_name'].label == 'full name')
+
+    def test_full_name_field_help_text(self):
+        form = SignUpForm()
+        self.assertEqual(form.fields['full_name'].help_text, 'Required. 100 characters or fewer.')
+
+    def test_email_field_validity(self):
+        email = 'abcdefg'
+        form = SignUpForm(data={'email': email})
+        self.assertFalse(form.is_valid())
+
+    def test_phone_no_max(self):
+        phoneno = 'asiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvrrsdfssf'
+        form = SignUpForm(data={'phone_no': phoneno})
+        self.assertFalse(form.is_valid())
+
+
+class ContactFormTest(TestCase):
+    def test_name_field_label(self):
+        form = ContactForm()
+        self.assertTrue(form.fields['name'].label is None or form.fields['name'].label == 'Name')
+
+    def test_name_field_max_length(self):
+        form = ContactForm()
+        self.assertEqual(form.fields['name'].max_length, 500)
+
+    def test_messaage_field_validity(self):
+        message = 'abcdefg'
+        form = ContactForm(data={'message': message})
+        self.assertFalse(form.is_valid())
+
+
+class EditFormTest(TestCase):
+    def test_full_name_field_label(self):
+        form = EditForm()
+        self.assertTrue(form.fields['full_name'].label is None or form.fields['full_name'].label == 'full name')
+
+    def test_email_field_validity(self):
+        email = 'abcdefg'
+        form = EditForm(data={'email': email})
+        self.assertFalse(form.is_valid())
+
+    def test_phone_no_max(self):
+        phoneno = 'asiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvasiuhasjjvrrsdfssf'
+        form = EditForm(data={'phone_no': phoneno})
+        self.assertFalse(form.is_valid())
+
+    def test_email_field_help_text(self):
+        form = EditForm()
+        self.assertEqual(form.fields['email'].help_text, 'Required.')
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,118 @@
 from django.test import TestCase
+from .forms import *
 
-# Create your tests here.
+
+class ProductModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Products.objects.create(name='book1', description='sharbhrbhbh', category='Books/Notes', price=100)
+
+    def test_name_label(self):
+        product = Products.objects.get(id=1)
+        field_label = product._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_description_label(self):
+        product = Products.objects.get(id=1)
+        field_label = product._meta.get_field('description').verbose_name
+        self.assertEqual(field_label, 'description')
+
+    def test_description_max_length(self):
+        product = Products.objects.get(id=1)
+        max_length = product._meta.get_field('description').max_length
+        self.assertEqual(max_length, 2083)
+
+
+class WishlistModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Wishlist.objects.create(username='abcd', pid=1)
+
+    def test_username_label(self):
+        wishlist = Wishlist.objects.get(id=1)
+        field_label = wishlist._meta.get_field('username').verbose_name
+        self.assertEqual(field_label, 'username')
+
+    def test_pid_label(self):
+        wishlist = Wishlist.objects.get(id=1)
+        field_label = wishlist._meta.get_field('pid').verbose_name
+        self.assertEqual(field_label, 'pid')
+
+    def test_username_max_length(self):
+        wishlist = Wishlist.objects.get(id=1)
+        max_length = wishlist._meta.get_field('username').max_length
+        self.assertEqual(max_length, 255)
+
+
+class TransactionModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Transaction.objects.create(buyer='abcd', seller='wxyz', pid=1, pname='pqr')
+
+    def test_buyer_label(self):
+        transaction = Transaction.objects.get(id=1)
+        field_label = transaction._meta.get_field('buyer').verbose_name
+        self.assertEqual(field_label, 'buyer')
+
+    def test_pid_label(self):
+        transaction = Transaction.objects.get(id=1)
+        field_label = transaction._meta.get_field('pid').verbose_name
+        self.assertEqual(field_label, 'pid')
+
+    def test_buyer_max_length(self):
+        transaction = Transaction.objects.get(id=1)
+        max_length = transaction._meta.get_field('buyer').max_length
+        self.assertEqual(max_length, 255)
+
+
+class RequestModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Requests.objects.create(buyer='abcd', seller='wxyz', pid=1)
+
+    def test_buyer_label(self):
+        requests = Requests.objects.get(id=1)
+        field_label = requests._meta.get_field('buyer').verbose_name
+        self.assertEqual(field_label, 'buyer')
+
+    def test_pid_label(self):
+        requests = Requests.objects.get(id=1)
+        field_label = requests._meta.get_field('pid').verbose_name
+        self.assertEqual(field_label, 'pid')
+
+    def test_buyer_max_length(self):
+        requests = Requests.objects.get(id=1)
+        max_length = requests._meta.get_field('buyer').max_length
+        self.assertEqual(max_length, 255)
+
+
+class ItemFormTest(TestCase):
+    def test_name_field_label(self):
+        form = Item()
+        self.assertTrue(form.fields['name'].label is None or form.fields['name'].label == 'Product Title')
+
+    def test_description_field_label(self):
+        form = Item()
+        self.assertTrue(form.fields['description'].label is None or form.fields['description'].label == 'Product Description')
+
+    def test_sell_field_choices(self):
+        form = Item()
+        self.assertEqual(form.fields['sell'].choices, SELL_CHOICES)
+
+
+class EditItemFormTest(TestCase):
+    def test_name_field_label(self):
+        form = Edit_Item()
+        self.assertTrue(form.fields['name'].label is None or form.fields['name'].label == 'Edit Name')
+
+    def test_description_field_label(self):
+        form = Edit_Item()
+        self.assertTrue(form.fields['description'].label is None or form.fields['description'].label == 'Edit Description')
+
+    def test_sell_field_choices(self):
+        form = Edit_Item()
+        self.assertEqual(form.fields['sell'].choices, SELL_CHOICES)
